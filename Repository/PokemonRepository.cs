176c46cb2a30965a7bedf0ace558d83f156625a4
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-03-15 23:11:14 +0100

    Add files via upload

---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,17 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interface;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) { 
+        }
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(propa => propa.Id).ToList();
+        }
+    }
+}
